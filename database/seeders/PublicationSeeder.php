218e06a22bd5bf708922a1160a8277f98ecec109
«Jordi» <jordi.mallafre@hotmail.com>
2022-10-20 15:58:37 +0200

    controler publicacio

--- database/seeders/PublicationSeeder.php
+++ database/seeders/PublicationSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Publication;
 
 class PublicationSeeder extends Seeder
 {
@@ -14,6 +15,129 @@ class PublicationSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $publications = [
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 0,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 1,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 0,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 2,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 2,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 3,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 0,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 4,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 4,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 5,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 7,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 6,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 5,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 7,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 14,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 8,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 23,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 9,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 31,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 10,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 0,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 11,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 28,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 12,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 19,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 13,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 37,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 14,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 16,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 15,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 47,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 16,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 35,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 17,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 0,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 18,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 10,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 19,
+            ],
+            [
+                'url'  => 'url_image/clouddown_icon-icons.com_54405.png',
+                'likes'  => 50,
+                'description'    => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare suscipit libero.',
+                'user_id'  => 20,
+            ],
+        ];
+
+        Publication::insert($publications);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,140 +16,140 @@ class UserSeeder extends Seeder
     public function run()
     {
         $users = [
-            [                
+            [
                 'name'  => 'Marco',
                 'surname'  => 'Torrent Lastra',
                 'username'    => 'marcotl',
                 'email' => 'marcotl@gmail.com',
                 'password'    => '7w2^v@2&Ik6m',
             ],
-            [                
+            [
                 'name'  => 'Rodrigo',
                 'surname'  => 'Caparrós Sanmartín',
                 'username'    => 'rodrigocs',
                 'email' => 'rodrigocs@gmail.com',
                 'password'    => 'EZ9%5Z7rp70o',
             ],
-            [                
+            [
                 'name'  => 'Claudia',
                 'surname'  => 'Varela Oliver',
                 'username'    => 'claudiavo',
                 'email' => 'claudiavo@gmail.com',
                 'password'    => '9YI7z%&R^TK8',
             ],
-            [                
+            [
                 'name'  => 'Lorena',
                 'surname'  => 'Jordá Losa',
                 'username'    => 'lorenajl',
                 'email' => 'lorenajl@gmail.com',
                 'password'    => '48vp0Cg^^DJK',
             ],
-            [                
+            [
                 'name'  => 'Remigio',
                 'surname'  => 'Pineda Arce',
                 'username'    => 'remigiopa',
                 'email' => 'remigiopa@gmail.com',
                 'password'    => 'V9d!v%se224y',
             ],
-            [                
+            [
                 'name'  => 'Jordi',
                 'surname'  => 'Paz Múñiz',
                 'username'    => 'jordipm',
                 'email' => 'jordipm@gmail.com',
                 'password'    => '&G47h9K01spb',
             ],
-            [                
+            [
                 'name'  => 'Alonso',
                 'surname'  => 'Iglesias Ibañez',
                 'username'    => 'alonsoii',
                 'email' => 'alonsoii@gmail.com',
                 'password'    => 'PnTnm^9Nr17F',
             ],
-            [                
+            [
                 'name'  => 'Jenny',
                 'surname'  => 'Planas Campillo',
                 'username'    => 'jennypc',
                 'email' => 'jennypc@gmail.com',
                 'password'    => 'yAHi0593Wm!g',
             ],
-            [                
+            [
                 'name'  => 'Gregorio',
                 'surname'  => 'Salom Barrios',
                 'username'    => 'gregoriosb',
                 'email' => 'gregoriosb@gmail.com',
                 'password'    => '8%q7lv032RdR',
             ],
-            [                
+            [
                 'name'  => 'Begoña',
                 'surname'  => 'Barbero Gutiérrez',
                 'username'    => 'begoñabg',
                 'email' => 'begoñabg@gmail.com',
                 'password'    => '7&8q8%vZVCil',
             ],
-            [                
+            [
                 'name'  => 'Imelda',
                 'surname'  => 'Saez Carpio',
                 'username'    => 'imeldasc',
                 'email' => 'imeldasc@gmail.com',
                 'password'    => '@dGzFJz!907S',
             ],
-            [                
+            [
                 'name'  => 'Úrsula',
                 'surname'  => 'Dalmau Barrena',
                 'username'    => 'ursuladb',
                 'email' => 'ursuladb@gmail.com',
                 'password'    => 'igF1kP&R&8W5',
             ],
-            [                
+            [
                 'name'  => 'Marcos',
                 'surname'  => 'Burgos Cózar',
                 'username'    => 'marcosbc',
                 'email' => 'marcosbc@gmail.com',
                 'password'    => '4000JLWT#qht',
             ],
-            [                
+            [
                 'name'  => 'Sebastián',
                 'surname'  => 'Cabeza Murillo',
                 'username'    => 'sebastiancm',
                 'email' => 'sebastiancm@gmail.com',
                 'password'    => '*uab311Ccdli',
             ],
-            [                
+            [
                 'name'  => 'Alex',
                 'surname'  => 'Corbacho Reguera',
                 'username'    => 'alexcr',
                 'email' => 'alexcr@gmail.com',
                 'password'    => '7D73$9aaEg^0',
             ],
-            [                
+            [
                 'name'  => 'Ainara',
                 'surname'  => 'Figuerola Gras',
                 'username'    => 'ainarafg',
                 'email' => 'ainarafg@gmail.com',
                 'password'    => 'qM9YCz0@%06d',
             ],
-            [                
+            [
                 'name'  => 'Nico',
                 'surname'  => 'Cabrera Grande',
                 'username'    => 'nicocg',
                 'email' => 'nicocg@gmail.com',
                 'password'    => 'Dg4b35*o2#QD',
             ],
-            [                
+            [
                 'name'  => 'Aarón',
                 'surname'  => 'Bernad Malo',
                 'username'    => 'aaronbm',
                 'email' => 'aaronbm@gmail.com',
                 'password'    => '3LR5A4$6EQzL',
             ],
-            [                
+            [
                 'name'  => 'Ismael',
                 'surname'  => 'Ramis Bernad',
                 'username'    => 'ismaelrb',
                 'email' => 'ismaelrb@gmail.com',
                 'password'    => '#zB55D3N*5%8',
             ],
-            [                
+            [
                 'name'  => 'Mireia',
                 'surname'  => 'Salinas Lozano',
                 'username'    => 'mireiasl',

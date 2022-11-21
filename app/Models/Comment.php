78ebe5fa640c7c406ee114c505cef6cfce6745dc
Francesc Navarro <cesccat82@gmail.com>
2022-11-21 12:56:23 +0100

    routes

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,17 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
 
     public function publication()
     {
         return $this->belongsTo(Publication::class);
-=======
-    protected $fillable = ['description', 'like', 'user_id', 'publication_id'];
+    }
+    
 
     public function user()
     {
         return $this->belongsTo(User::class);
->>>>>>> 453635e7c0497fe29c5bfb558000a5aa1124514f
     }
 }

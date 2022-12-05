c793d50c05e44d01e32350d419b85136655ec276
Francesc Navarro <cesccat82@gmail.com>
2022-12-05 13:03:35 +0100

    últims canvis

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -61,7 +62,7 @@ class UserController extends Controller
         $user = User::findOrFail($id);
         $user->name = $request->name;
         $user->surname = $request->surname;
-        $user->username = $request->username;
+        $user->username = Str::lower($request->username);
         $user->save();
         return view('users.show', compact('user'));
     }
